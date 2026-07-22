71e877aa11738cdbe1c94ebb4699672b7623c2f1
Fatejian <Fatejian@users.noreply.github.com>
2026-07-22 10:04:17 +0000

    deploy: 4c3ef5f83138ba15acad47c82247af2a44800753

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.B0Srp_uf.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.CNYsqdWQ.js";import{_ as a}from"./mermaid.core.MZuGn6MW.js";import"./chunk-5VM5RSS4.DJxln6w5.js";import"./chunk-XXDRQBXY.BA4t-vy0.js";import"./chunk-VR4S4FIN.C0noqGmO.js";import"./chunk-32BRIVSS.CUWEXPam.js";import"./channel.txfiL4_R.js";import"./framework.Bd3Jtoau.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
